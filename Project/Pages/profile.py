2529578c192d87bdae483c43d2cf4b3014a0b69a
FusionTeamQA <qatestfusion@gmail.com>
2022-12-16 16:15:48 +0300

    Edit test profile

--- Project/Pages/profile.py
+++ Project/Pages/profile.py
@@ -10,7 +10,6 @@ from Project.Locators.locator import Profile
 class TestProfile(Base):
 
     def OpenProfile(self):
-
         Email_Stack = "eld.rus@rambler.ru"
         Pass_Stack = "AAAqqq111"
         self.element_is_visible(Login_page_Locators.LOGIN_LINK).click()
@@ -21,18 +20,20 @@ class TestProfile(Base):
         self.element_is_visible(Profile.PROFILE_AVATAR).click()
 
     def resilt_profile_page(self):
-
         result = self.element_is_present(Profile.WELCOME_TEXT_PROFILE).text
         return result
 
     def EditProfile(self):
-
         num = random.random()
         self.element_is_visible(Profile.EDIT_PROFILE_BTN).click()
-        self.element_is_visible(Profile.PROFILE_DISPLAY_NAME).send_keys("AutoTest")
+        self.element_is_visible(Profile.PROFILE_LOCATION).clear()
         self.element_is_visible(Profile.PROFILE_LOCATION).send_keys("Selenium")
+        self.element_is_visible(Profile.PROFILE_TITLE).clear()
         self.element_is_visible(Profile.PROFILE_TITLE).send_keys("Write Selenium test")
+        self.element_is_visible(Profile.PROFILE_ABOUT_ME).clear()
         self.element_is_visible(Profile.PROFILE_ABOUT_ME).send_keys("About me testcase")
+        self.element_is_visible(Profile.PROFILE_WEB_SITE_LINK).clear()
         self.element_is_visible(Profile.PROFILE_WEB_SITE_LINK).send_keys("google.com")
+        self.element_is_visible(Profile.PROFILE_FULL_NAME).clear()
         self.element_is_visible(Profile.PROFILE_FULL_NAME).send_keys("Auto Test Selenium Python")
         self.element_is_visible(Profile.PROFILE_SAVE_BUTTON).click()
